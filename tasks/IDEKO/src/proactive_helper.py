fde01502ca0a062e67b5c2802e51e5ecdce93328
Calvo, Albert <albert.calvo@i2cat.net>
2024-07-17 14:21:41 +0200

    Rename dataset to artifact definition in proactive_helper

--- tasks/IDEKO/src/proactive_helper.py
+++ tasks/IDEKO/src/proactive_helper.py
@@ -1,4 +1,3 @@
-
 import os
 import sys
 import pickle
@@ -6,37 +5,37 @@ import json
 import numpy as np
 
 
-def save_datasets(variables, *data):
+def save_artifacts(variables, *data):
     for (key, value) in data:
-        save_dataset(variables, key, value)
+        save_artifact(variables, key, value)
 
 
-def load_datasets(variables, *keys):
-    return [load_dataset(variables, key) for key in keys]
+def load_artifacts(variables, *keys):
+    return [load_artifact(variables, key) for key in keys]
 
 
-def save_dataset(variables, key, value):
+def save_artifact(variables, key, value):
     value_size = sys.getsizeof(value)
     print(f"Saving output data of size {value_size} with key {key}")
     if value_size > 100:
-        print("Saving as large dataset")
-        save_large_dataset(variables, key, value)
+        print("Saving as large artifact")
+        save_large_artifact(variables, key, value)
     else:
         print("Saving using the 'variables' object (internal database)")
         variables.put(key, value)
 
 
-def load_dataset(variables, key):
+def load_artifact(variables, key):
     print(f"Loading input data with key {key}")
     if key in variables:
         print("Loading using the 'variables' object (internal database)")
         return variables.get(key)
     else:
-        print("Loading as large dataset")
-        return load_large_dataset(variables, key)
+        print("Loading as large artifact")
+        return load_large_artifact(variables, key)
 
 
-def save_large_dataset(variables, key, value):
+def save_large_artifact(variables, key, value):
     job_id = variables.get("PA_JOB_ID")
     task_id = variables.get("PA_TASK_ID")
     task_folder = os.path.join("/shared", job_id, task_id)
@@ -47,7 +46,7 @@ def save_large_dataset(variables, key, value):
     variables.put("PREVIOUS_TASK_ID", str(task_id))
 
 
-def load_large_dataset(variables, key):
+def load_large_artifact(variables, key):
     job_id = variables.get("PA_JOB_ID")
     task_id = variables.get("PREVIOUS_TASK_ID")
     task_folder = os.path.join("/shared", job_id, task_id)
